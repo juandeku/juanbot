ae9b67616c008464371f77e16cdd041e4055a364
juandeku <76886000+juandeku@users.noreply.github.com>
2021-01-03 13:14:08 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# bot de figurinha simplezão KKKKK
---
+++ help.js
@@ -0,0 +1,15 @@
+const help = () => {
+	return '> ```comandos do bot de figurinha``` <\n' +
+	'comando : *!sticker* or *!stiker*\n' +
+	'descrição : converte uma imagem, gif ou vídeo para figurinha\n' +
+	'> ```comando de áudio (tts)``` <\n' +
+	'comando : *!gtts*\n' +
+	'descrição : converte sua mensagem para um áudio. para usar, dê um !gtts, o idioma e o texto.\n' +
+	'exemplo: *!gtts* *pt* macaco falante\n
+	
+	'*aproveite e divirta-se ;)*
+	' ~dek'
+
+}
+
+exports.help = help
